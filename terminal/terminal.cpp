1b186a2ddf72bb65a61f0dd53a6c2c39990a2df4
Dumitru-Asandei <52822996+NikolaTesla13@users.noreply.github.com>
2020-02-24 13:12:59 -0500

    New version!
    Color text.
    Show path.
    Help commands.
    Multiple arguments.
    Working g++.

--- terminal/terminal.cpp
+++ terminal/terminal.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <bits/stdc++.h>
 
 
 using namespace std;
@@ -10,23 +6,77 @@ ifstream read("data.in");
 ofstream write("data.in", ios_base::app);
 
 
+
+
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+
+
+char msg[1000];
+string ver,usr,pwd,ac;
+
 void about()
 {
 	cout << "TerminalS v0.1 BETA\n";
 }
 
+void dev_help()
+{
+    cout<< RED << "DEVELOPER HELP\n";
+    cout<< BOLDYELLOW << "HOW TO BUILD\nEVERYTHING\n\n";
+    cout<< WHITE << "g++ - C++\npython3 - Python\ngcc - C\njava - Java\nflutter run - Flutter\n\n";
+}
+
+void nav_help()
+{
+    cout<< RED << "NEWBIE HELP TO TERMINAL\n";
+    cout<< BOLDYELLOW << "HOW TO USE\nIT\n\n";
+    cout<< WHITE << "cd folder - navigate to the folder\npwd - show the path\nmkdir folder - make a new folder\ncd .. - navigate one level down\ntouch file - create file\nls - show the file in the currend directory\n\n";
+}
 
 void check(char msg[1000])
 {
 	if (strcmp(msg,"about")==0)
 		about();
 	else
-		if (strcmp(msg,"exit")==0)
-			system("pause");
+		if (strcmp(msg,"dev-help")==0)
+			dev_help();
+        else
+            if (strcmp(msg,"help")==0)
+                nav_help();
 		else
 			system(msg);
 }
 
+
+void terminal()
+{
+	string pwd = "";
+	while (strcmp(msg,"exit")!=0)
+	{
+        cout<< CYAN;
+		pwd = system("pwd");
+		cout << CYAN << pwd << CYAN<< "$  " << GREEN;
+		cin.getline(msg, 100);
+		check(msg);
+	}
+}
 
 void create_user()
 {
@@ -38,40 +88,43 @@ void create_user()
 
 	write << usr1 << " " << pwd1 << endl;
 }
-char msg[1000];
-string ver,usr,pwd,ac;
+
+void login()
+{
+    cout << "Do you have an account? y/n\n";
+        cin >> ac;
+        if (ac == "n") {
+            create_user();
+            terminal();
+        }
+        if(ac == "y")
+        {
+            cout << "Username: ";
+            cin >> usr;
+            cout << "\nPassword: ";
+            cin >> pwd;
+            while (getline(read, ver))
+                if (usr + " " + pwd != ver)
+                    ;
+                else
+                    terminal();
+        }
+}
+
+void debug()
+{
+	while (strcmp(msg,"exit")!=0)
+	{
+		cin.getline(msg, 100);
+		cout<<endl;
+		cout<<msg;
+	}
+}
 int main()
 {
-	//system("Color 0A");
-	cout << "Do you have an account? y/n\n";
-	cin >> ac;
-	if (ac == "n") {
-		create_user();
-		while (strcmp(msg,"exit")!=0)
-		{
-			
-			cout << ">> ";
-			cin >> msg;
-			check(msg);
-		}
-	}
-	if(ac == "y")
-	{
-		cout << "Username: ";
-		cin >> usr;
-		cout << "\nPassword: ";
-		cin >> pwd;
-		while (getline(read, ver))
-			if (usr + " " + pwd != ver)
-				cout << "";
-			else
-				while (strcmp(msg, "exit") != 0)
-				{
-					cout << ">> ";
-					cin >> msg;
-					check(msg);
-				}
-	}
+    //login() USELESS
+	terminal();
+	//debug();
 	read.close();
 	write.close();
 	return 0;
